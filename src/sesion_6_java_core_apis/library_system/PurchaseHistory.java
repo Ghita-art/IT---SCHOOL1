fc57f016938f63002af4c589784f091f6ea37d53
Ghita <iceman57187@gmail.com>
2024-06-21 00:18:12 +0300

    Session 6

---
+++ src/sesion_6_java_core_apis/library_system/PurchaseHistory.java
@@ -0,0 +1,9 @@
+package sesion_6_java_core_apis.library_system;
+
+import java.util.List;
+
+public class PurchaseHistory {
+
+        private List<Product> products;
+    }
+
---
+++ src/sesion_6_java_core_apis/library_system/User.java
@@ -0,0 +1,11 @@
+package sesion_6_java_core_apis.library_system;
+
+import java.time.LocalDate;
+
+public class User {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private LocalDate birthday;
+}
