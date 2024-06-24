4849a14b6d9d059d96334b0f1afad184034da971
Ghita <iceman57187@gmail.com>
2024-06-24 17:53:50 +0300

    Session 6

---
+++ src/sesion_6_java_core_apis/challenges/Challenge_2.java
@@ -0,0 +1,16 @@
+package sesion_6_java_core_apis.challenges;
+
+public class Challenge_2 {
+    public static boolean isPolindrome(String str){
+        StringBuilder reverse = new StringBuilder(str).reverse();
+        return str.equals(reverse.toString());
+    }
+    public static void main(String[] args) {
+        String text = "racecar";
+        boolean isPolindrome = isPolindrome(text);
+        System.out.println(text +" is polindrome: " + isPolindrome);
+        text = "racecar";
+        isPolindrome = isPolindrome(text);
+        System.out.println(text +" is polindrome: " + isPolindrome);
+    }
+}
