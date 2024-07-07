a5c79e6992043366a9f429801b9a64804f43caa4
Ghita <iceman57187@gmail.com>
2024-07-07 18:00:33 +0300

    Session 6

--- src/session_9_arrays/challenges/Challenge_11.java
+++ src/session_9_arrays/challenges/Challenge_11.java
@@ -1,68 +1,55 @@
 package session_9_arrays.challenges;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Challenge_11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double num1, num2;
-        char operator;
-        do {
-            num1 = getDouble(scanner, "Enter first number: ");
-            num2 = getDouble(scanner, "Enter second number: ");
-            operator = getChar(scanner, "Enter an operator (+, -, *, /): ");
-            double result = calculate(num1, num2, operator);
-            System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
-            System.out.print("Do you want to perform another calculation? (y/n): ");
-        } while (scanner.nextLine().equalsIgnoreCase("y"));
+        boolean exit = false;
 
-        System.out.println("Exiting calculator...");
-        scanner.close();
-    }
+        while (!exit) {
+            System.out.println("Please enter the first number: ");
+            double num1 = scanner.nextDouble();
 
-    public static double getDouble(Scanner scanner, String message) {
-        double number;
-        do {
-            System.out.print(message);
-            try {
-                number = scanner.nextDouble();
-                return number;
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please enter a number.");
-                scanner.nextLine(); // Clear the scanner buffer
+            System.out.println("Please enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("Please enter the operation (+, -, *, /.): ");
+            System.out.println("+");
+            System.out.println("-");
+            System.out.println("*");
+            System.out.println("/");
+            System.out.println("Enter your choice: ");
+            String choice = scanner.next();
+
+            switch (choice) {
+                case "+":
+                    System.out.println("Result = " + num1 + " + " + num2 + " = " + (num1 + num2));
+                    break;
+                case "-":
+                    System.out.println("Result = " + num1 + " - " + num2 + " = " + (num1 - num2));
+                    break;
+                case "*":
+                    System.out.println("Result = " + num1 + " * " + num2 + " = " + (num1 * num2));
+                    break;
+                case "/":
+                    System.out.println("Result = " + num1 + " / " + num2 + " = " + (num1 / num2));
+                    break;
+                default:
+                    System.out.println("The format is not the one we expect");
+                    break;
             }
-        } while (true);
-    }
 
-    // Function to get a character from the user with input validation
-    public static char getChar(Scanner scanner, String message) {
-        char operator;
-        do {
-            System.out.print(message);
-            String input = scanner.nextLine();
-            if (input.length() != 1 || !"+-*/".contains(input)) {
-                System.out.println("Invalid operator. Please enter +, -, *, or /.");
-            } else {
-                operator = input.charAt(0);
-                return operator;
+            System.out.println("Do you want to continue? (y or n)");
+            String ch = scanner.next();
+
+            if (!ch.equalsIgnoreCase("y")) {
+                exit = true;
             }
-        } while (true);
-    }
 
-    public static double calculate(double num1, double num2, char operator) {
-        switch (operator) {
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            case '*':
-                return num1 * num2;
-            case '/':
-                if (num2 == 0) {
-                    throw new ArithmeticException("Division by zero is not allowed.");
-                }
-                return num1 / num2;
-            default:
-                throw new IllegalArgumentException("Invalid operator: " + operator);
         }
+        System.out.println("Calculator ended.");
+        scanner.close();
     }
 }
--- src/session_9_arrays/challenges/Challenge_3.java
+++ src/session_9_arrays/challenges/Challenge_3.java
@@ -7,7 +7,7 @@ public class Challenge_3 {
         ArrayList<String> studentList = new ArrayList<String>(10);
         ArrayList<String> graduateStudentList = new ArrayList<String>(10);
         for (int i = 0; i < 10; i++) {
-            studentList.add("Student" + (i + 1));
+            studentList.add("Student " + (i + 1));
 
         }
         graduateStudentList.addAll(studentList);
