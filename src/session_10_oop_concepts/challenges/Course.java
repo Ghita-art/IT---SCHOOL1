388aa1845c658b824cfb1ddcf2e3496af722e859
Ghita <iceman57187@gmail.com>
2024-07-10 19:57:54 +0300

    Coding Kata

---
+++ src/session_10_oop_concepts/challenges/Course.java
@@ -0,0 +1,13 @@
+package session_10_oop_concepts.challenges;
+
+public class Course {
+    public String math;
+    public String CS;
+    public String physics;
+    public String chemistry;
+    public String biology;
+    public String geography;
+    public String english;
+    public String history;
+
+}
---
+++ src/session_10_oop_concepts/challenges/Person.java
@@ -0,0 +1,18 @@
+package session_10_oop_concepts.challenges;
+
+public class Person {
+    private String firstName;
+    private String secondName;
+    int age;
+    private String sex;
+    private String dateOfBirth;
+    private String uniqueIdentifier;
+    private String address;
+
+    public void Student(String firstName, String secondName, int age,String sex, String dateOfBirth, String uniqueIdentifier, String address){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+
+    }
+}
